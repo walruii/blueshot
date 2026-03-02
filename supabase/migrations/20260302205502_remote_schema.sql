d75cbf8d5750e934e51b26c2464eb609043f9e9d
walruii <walruiix@gmail.com>
2026-03-03 03:11:07 +0530

    closes #45

---
+++ supabase/migrations/20260302205502_remote_schema.sql
@@ -0,0 +1,583 @@
+revoke delete on table "public"."account" from "anon";
+
+revoke insert on table "public"."account" from "anon";
+
+revoke references on table "public"."account" from "anon";
+
+revoke select on table "public"."account" from "anon";
+
+revoke trigger on table "public"."account" from "anon";
+
+revoke truncate on table "public"."account" from "anon";
+
+revoke update on table "public"."account" from "anon";
+
+revoke delete on table "public"."account" from "authenticated";
+
+revoke insert on table "public"."account" from "authenticated";
+
+revoke references on table "public"."account" from "authenticated";
+
+revoke select on table "public"."account" from "authenticated";
+
+revoke trigger on table "public"."account" from "authenticated";
+
+revoke truncate on table "public"."account" from "authenticated";
+
+revoke update on table "public"."account" from "authenticated";
+
+revoke delete on table "public"."conversation" from "anon";
+
+revoke insert on table "public"."conversation" from "anon";
+
+revoke references on table "public"."conversation" from "anon";
+
+revoke select on table "public"."conversation" from "anon";
+
+revoke trigger on table "public"."conversation" from "anon";
+
+revoke truncate on table "public"."conversation" from "anon";
+
+revoke update on table "public"."conversation" from "anon";
+
+revoke delete on table "public"."conversation" from "authenticated";
+
+revoke insert on table "public"."conversation" from "authenticated";
+
+revoke references on table "public"."conversation" from "authenticated";
+
+revoke select on table "public"."conversation" from "authenticated";
+
+revoke trigger on table "public"."conversation" from "authenticated";
+
+revoke truncate on table "public"."conversation" from "authenticated";
+
+revoke update on table "public"."conversation" from "authenticated";
+
+revoke delete on table "public"."conversation_participant" from "anon";
+
+revoke insert on table "public"."conversation_participant" from "anon";
+
+revoke references on table "public"."conversation_participant" from "anon";
+
+revoke select on table "public"."conversation_participant" from "anon";
+
+revoke trigger on table "public"."conversation_participant" from "anon";
+
+revoke truncate on table "public"."conversation_participant" from "anon";
+
+revoke update on table "public"."conversation_participant" from "anon";
+
+revoke delete on table "public"."conversation_participant" from "authenticated";
+
+revoke insert on table "public"."conversation_participant" from "authenticated";
+
+revoke references on table "public"."conversation_participant" from "authenticated";
+
+revoke select on table "public"."conversation_participant" from "authenticated";
+
+revoke trigger on table "public"."conversation_participant" from "authenticated";
+
+revoke truncate on table "public"."conversation_participant" from "authenticated";
+
+revoke update on table "public"."conversation_participant" from "authenticated";
+
+revoke delete on table "public"."event" from "anon";
+
+revoke insert on table "public"."event" from "anon";
+
+revoke references on table "public"."event" from "anon";
+
+revoke select on table "public"."event" from "anon";
+
+revoke trigger on table "public"."event" from "anon";
+
+revoke truncate on table "public"."event" from "anon";
+
+revoke update on table "public"."event" from "anon";
+
+revoke delete on table "public"."event" from "authenticated";
+
+revoke insert on table "public"."event" from "authenticated";
+
+revoke references on table "public"."event" from "authenticated";
+
+revoke select on table "public"."event" from "authenticated";
+
+revoke trigger on table "public"."event" from "authenticated";
+
+revoke truncate on table "public"."event" from "authenticated";
+
+revoke update on table "public"."event" from "authenticated";
+
+revoke delete on table "public"."event_access" from "anon";
+
+revoke insert on table "public"."event_access" from "anon";
+
+revoke references on table "public"."event_access" from "anon";
+
+revoke select on table "public"."event_access" from "anon";
+
+revoke trigger on table "public"."event_access" from "anon";
+
+revoke truncate on table "public"."event_access" from "anon";
+
+revoke update on table "public"."event_access" from "anon";
+
+revoke delete on table "public"."event_access" from "authenticated";
+
+revoke insert on table "public"."event_access" from "authenticated";
+
+revoke references on table "public"."event_access" from "authenticated";
+
+revoke select on table "public"."event_access" from "authenticated";
+
+revoke trigger on table "public"."event_access" from "authenticated";
+
+revoke truncate on table "public"."event_access" from "authenticated";
+
+revoke update on table "public"."event_access" from "authenticated";
+
+revoke delete on table "public"."event_group" from "anon";
+
+revoke insert on table "public"."event_group" from "anon";
+
+revoke references on table "public"."event_group" from "anon";
+
+revoke select on table "public"."event_group" from "anon";
+
+revoke trigger on table "public"."event_group" from "anon";
+
+revoke truncate on table "public"."event_group" from "anon";
+
+revoke update on table "public"."event_group" from "anon";
+
+revoke delete on table "public"."event_group" from "authenticated";
+
+revoke insert on table "public"."event_group" from "authenticated";
+
+revoke references on table "public"."event_group" from "authenticated";
+
+revoke select on table "public"."event_group" from "authenticated";
+
+revoke trigger on table "public"."event_group" from "authenticated";
+
+revoke truncate on table "public"."event_group" from "authenticated";
+
+revoke update on table "public"."event_group" from "authenticated";
+
+revoke delete on table "public"."event_group_access" from "anon";
+
+revoke insert on table "public"."event_group_access" from "anon";
+
+revoke references on table "public"."event_group_access" from "anon";
+
+revoke select on table "public"."event_group_access" from "anon";
+
+revoke trigger on table "public"."event_group_access" from "anon";
+
+revoke truncate on table "public"."event_group_access" from "anon";
+
+revoke update on table "public"."event_group_access" from "anon";
+
+revoke delete on table "public"."event_group_access" from "authenticated";
+
+revoke insert on table "public"."event_group_access" from "authenticated";
+
+revoke references on table "public"."event_group_access" from "authenticated";
+
+revoke select on table "public"."event_group_access" from "authenticated";
+
+revoke trigger on table "public"."event_group_access" from "authenticated";
+
+revoke truncate on table "public"."event_group_access" from "authenticated";
+
+revoke update on table "public"."event_group_access" from "authenticated";
+
+revoke delete on table "public"."event_user_state" from "anon";
+
+revoke insert on table "public"."event_user_state" from "anon";
+
+revoke references on table "public"."event_user_state" from "anon";
+
+revoke select on table "public"."event_user_state" from "anon";
+
+revoke trigger on table "public"."event_user_state" from "anon";
+
+revoke truncate on table "public"."event_user_state" from "anon";
+
+revoke update on table "public"."event_user_state" from "anon";
+
+revoke delete on table "public"."event_user_state" from "authenticated";
+
+revoke insert on table "public"."event_user_state" from "authenticated";
+
+revoke references on table "public"."event_user_state" from "authenticated";
+
+revoke select on table "public"."event_user_state" from "authenticated";
+
+revoke trigger on table "public"."event_user_state" from "authenticated";
+
+revoke truncate on table "public"."event_user_state" from "authenticated";
+
+revoke update on table "public"."event_user_state" from "authenticated";
+
+revoke delete on table "public"."meeting" from "anon";
+
+revoke insert on table "public"."meeting" from "anon";
+
+revoke references on table "public"."meeting" from "anon";
+
+revoke select on table "public"."meeting" from "anon";
+
+revoke trigger on table "public"."meeting" from "anon";
+
+revoke truncate on table "public"."meeting" from "anon";
+
+revoke update on table "public"."meeting" from "anon";
+
+revoke delete on table "public"."meeting" from "authenticated";
+
+revoke insert on table "public"."meeting" from "authenticated";
+
+revoke references on table "public"."meeting" from "authenticated";
+
+revoke select on table "public"."meeting" from "authenticated";
+
+revoke trigger on table "public"."meeting" from "authenticated";
+
+revoke truncate on table "public"."meeting" from "authenticated";
+
+revoke update on table "public"."meeting" from "authenticated";
+
+revoke delete on table "public"."meeting_event" from "anon";
+
+revoke insert on table "public"."meeting_event" from "anon";
+
+revoke references on table "public"."meeting_event" from "anon";
+
+revoke select on table "public"."meeting_event" from "anon";
+
+revoke trigger on table "public"."meeting_event" from "anon";
+
+revoke truncate on table "public"."meeting_event" from "anon";
+
+revoke update on table "public"."meeting_event" from "anon";
+
+revoke delete on table "public"."meeting_event" from "authenticated";
+
+revoke insert on table "public"."meeting_event" from "authenticated";
+
+revoke references on table "public"."meeting_event" from "authenticated";
+
+revoke select on table "public"."meeting_event" from "authenticated";
+
+revoke trigger on table "public"."meeting_event" from "authenticated";
+
+revoke truncate on table "public"."meeting_event" from "authenticated";
+
+revoke update on table "public"."meeting_event" from "authenticated";
+
+revoke delete on table "public"."meeting_participant" from "anon";
+
+revoke insert on table "public"."meeting_participant" from "anon";
+
+revoke references on table "public"."meeting_participant" from "anon";
+
+revoke select on table "public"."meeting_participant" from "anon";
+
+revoke trigger on table "public"."meeting_participant" from "anon";
+
+revoke truncate on table "public"."meeting_participant" from "anon";
+
+revoke update on table "public"."meeting_participant" from "anon";
+
+revoke delete on table "public"."meeting_participant" from "authenticated";
+
+revoke insert on table "public"."meeting_participant" from "authenticated";
+
+revoke references on table "public"."meeting_participant" from "authenticated";
+
+revoke select on table "public"."meeting_participant" from "authenticated";
+
+revoke trigger on table "public"."meeting_participant" from "authenticated";
+
+revoke truncate on table "public"."meeting_participant" from "authenticated";
+
+revoke update on table "public"."meeting_participant" from "authenticated";
+
+revoke delete on table "public"."message" from "anon";
+
+revoke insert on table "public"."message" from "anon";
+
+revoke references on table "public"."message" from "anon";
+
+revoke select on table "public"."message" from "anon";
+
+revoke trigger on table "public"."message" from "anon";
+
+revoke truncate on table "public"."message" from "anon";
+
+revoke update on table "public"."message" from "anon";
+
+revoke delete on table "public"."message" from "authenticated";
+
+revoke insert on table "public"."message" from "authenticated";
+
+revoke references on table "public"."message" from "authenticated";
+
+revoke select on table "public"."message" from "authenticated";
+
+revoke trigger on table "public"."message" from "authenticated";
+
+revoke truncate on table "public"."message" from "authenticated";
+
+revoke update on table "public"."message" from "authenticated";
+
+revoke delete on table "public"."notification" from "anon";
+
+revoke insert on table "public"."notification" from "anon";
+
+revoke references on table "public"."notification" from "anon";
+
+revoke select on table "public"."notification" from "anon";
+
+revoke trigger on table "public"."notification" from "anon";
+
+revoke truncate on table "public"."notification" from "anon";
+
+revoke update on table "public"."notification" from "anon";
+
+revoke delete on table "public"."notification" from "authenticated";
+
+revoke insert on table "public"."notification" from "authenticated";
+
+revoke references on table "public"."notification" from "authenticated";
+
+revoke select on table "public"."notification" from "authenticated";
+
+revoke trigger on table "public"."notification" from "authenticated";
+
+revoke truncate on table "public"."notification" from "authenticated";
+
+revoke update on table "public"."notification" from "authenticated";
+
+revoke delete on table "public"."passkey" from "anon";
+
+revoke insert on table "public"."passkey" from "anon";
+
+revoke references on table "public"."passkey" from "anon";
+
+revoke select on table "public"."passkey" from "anon";
+
+revoke trigger on table "public"."passkey" from "anon";
+
+revoke truncate on table "public"."passkey" from "anon";
+
+revoke update on table "public"."passkey" from "anon";
+
+revoke delete on table "public"."passkey" from "authenticated";
+
+revoke insert on table "public"."passkey" from "authenticated";
+
+revoke references on table "public"."passkey" from "authenticated";
+
+revoke select on table "public"."passkey" from "authenticated";
+
+revoke trigger on table "public"."passkey" from "authenticated";
+
+revoke truncate on table "public"."passkey" from "authenticated";
+
+revoke update on table "public"."passkey" from "authenticated";
+
+revoke delete on table "public"."session" from "anon";
+
+revoke insert on table "public"."session" from "anon";
+
+revoke references on table "public"."session" from "anon";
+
+revoke select on table "public"."session" from "anon";
+
+revoke trigger on table "public"."session" from "anon";
+
+revoke truncate on table "public"."session" from "anon";
+
+revoke update on table "public"."session" from "anon";
+
+revoke delete on table "public"."session" from "authenticated";
+
+revoke insert on table "public"."session" from "authenticated";
+
+revoke references on table "public"."session" from "authenticated";
+
+revoke select on table "public"."session" from "authenticated";
+
+revoke trigger on table "public"."session" from "authenticated";
+
+revoke truncate on table "public"."session" from "authenticated";
+
+revoke update on table "public"."session" from "authenticated";
+
+revoke delete on table "public"."twoFactor" from "anon";
+
+revoke insert on table "public"."twoFactor" from "anon";
+
+revoke references on table "public"."twoFactor" from "anon";
+
+revoke select on table "public"."twoFactor" from "anon";
+
+revoke trigger on table "public"."twoFactor" from "anon";
+
+revoke truncate on table "public"."twoFactor" from "anon";
+
+revoke update on table "public"."twoFactor" from "anon";
+
+revoke delete on table "public"."twoFactor" from "authenticated";
+
+revoke insert on table "public"."twoFactor" from "authenticated";
+
+revoke references on table "public"."twoFactor" from "authenticated";
+
+revoke select on table "public"."twoFactor" from "authenticated";
+
+revoke trigger on table "public"."twoFactor" from "authenticated";
+
+revoke truncate on table "public"."twoFactor" from "authenticated";
+
+revoke update on table "public"."twoFactor" from "authenticated";
+
+revoke delete on table "public"."user" from "anon";
+
+revoke insert on table "public"."user" from "anon";
+
+revoke references on table "public"."user" from "anon";
+
+revoke select on table "public"."user" from "anon";
+
+revoke trigger on table "public"."user" from "anon";
+
+revoke truncate on table "public"."user" from "anon";
+
+revoke update on table "public"."user" from "anon";
+
+revoke delete on table "public"."user" from "authenticated";
+
+revoke insert on table "public"."user" from "authenticated";
+
+revoke references on table "public"."user" from "authenticated";
+
+revoke select on table "public"."user" from "authenticated";
+
+revoke trigger on table "public"."user" from "authenticated";
+
+revoke truncate on table "public"."user" from "authenticated";
+
+revoke update on table "public"."user" from "authenticated";
+
+revoke delete on table "public"."user_group" from "anon";
+
+revoke insert on table "public"."user_group" from "anon";
+
+revoke references on table "public"."user_group" from "anon";
+
+revoke select on table "public"."user_group" from "anon";
+
+revoke trigger on table "public"."user_group" from "anon";
+
+revoke truncate on table "public"."user_group" from "anon";
+
+revoke update on table "public"."user_group" from "anon";
+
+revoke delete on table "public"."user_group" from "authenticated";
+
+revoke insert on table "public"."user_group" from "authenticated";
+
+revoke references on table "public"."user_group" from "authenticated";
+
+revoke select on table "public"."user_group" from "authenticated";
+
+revoke trigger on table "public"."user_group" from "authenticated";
+
+revoke truncate on table "public"."user_group" from "authenticated";
+
+revoke update on table "public"."user_group" from "authenticated";
+
+revoke delete on table "public"."user_group_member" from "anon";
+
+revoke insert on table "public"."user_group_member" from "anon";
+
+revoke references on table "public"."user_group_member" from "anon";
+
+revoke select on table "public"."user_group_member" from "anon";
+
+revoke trigger on table "public"."user_group_member" from "anon";
+
+revoke truncate on table "public"."user_group_member" from "anon";
+
+revoke update on table "public"."user_group_member" from "anon";
+
+revoke delete on table "public"."user_group_member" from "authenticated";
+
+revoke insert on table "public"."user_group_member" from "authenticated";
+
+revoke references on table "public"."user_group_member" from "authenticated";
+
+revoke select on table "public"."user_group_member" from "authenticated";
+
+revoke trigger on table "public"."user_group_member" from "authenticated";
+
+revoke truncate on table "public"."user_group_member" from "authenticated";
+
+revoke update on table "public"."user_group_member" from "authenticated";
+
+revoke delete on table "public"."verification" from "anon";
+
+revoke insert on table "public"."verification" from "anon";
+
+revoke references on table "public"."verification" from "anon";
+
+revoke select on table "public"."verification" from "anon";
+
+revoke trigger on table "public"."verification" from "anon";
+
+revoke truncate on table "public"."verification" from "anon";
+
+revoke update on table "public"."verification" from "anon";
+
+revoke delete on table "public"."verification" from "authenticated";
+
+revoke insert on table "public"."verification" from "authenticated";
+
+revoke references on table "public"."verification" from "authenticated";
+
+revoke select on table "public"."verification" from "authenticated";
+
+revoke trigger on table "public"."verification" from "authenticated";
+
+revoke truncate on table "public"."verification" from "authenticated";
+
+revoke update on table "public"."verification" from "authenticated";
+
+
+  create policy "Allow anon users to listen"
+  on "realtime"."messages"
+  as permissive
+  for select
+  to anon
+using (true);
+
+
+
+  create policy "Allow service_role to send"
+  on "realtime"."messages"
+  as permissive
+  for insert
+  to service_role
+with check (true);
+
+
+CREATE TRIGGER protect_buckets_delete BEFORE DELETE ON storage.buckets FOR EACH STATEMENT EXECUTE FUNCTION storage.protect_delete();
+
+CREATE TRIGGER protect_objects_delete BEFORE DELETE ON storage.objects FOR EACH STATEMENT EXECUTE FUNCTION storage.protect_delete();
+
+
